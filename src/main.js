e63d0f59df940e7c4c810707fac8df2baa41fa05
Fitz Ryland <fitzryland@gmail.com>
2020-02-16 18:09:29 -0800

    workin pretty well for now

--- src/main.js
+++ src/main.js
@@ -4,8 +4,6 @@ import store from "./store"
 import App from './App.vue'
 Vue.config.productionTip = false
 
-// var dataUrl = 'https://spreadsheets.google.com/feeds/list/1t9DT6g23nMERZVJ-pV3UdLnrnS6vKJLG78wn3sSkYTw/1/public/values?alt=json';
-
 const app = new Vue({
   store,
   router,
